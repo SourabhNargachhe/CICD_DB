8ac83aa1f3238dc57b574302afc959bf05213f0c
SourabhNargachhe <sourabh@insurancegig.com>
2023-06-15 21:17:14 +0530

    Initial commit

---
+++ CICD_DB/CICD_DB/dbo/Tables/CompanyInformation.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[CompanyInformation] (
+    [Id]      INT            NULL,
+    [Name]    NVARCHAR (255) NULL,
+    [Address] NVARCHAR (255) NULL,
+    [City]    NVARCHAR (255) NULL
+);
+
---
+++ CICD_DB/CICD_DB/dbo/Tables/Userinformation.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Userinformation] (
+    [Id]        INT            NULL,
+    [LastName]  NVARCHAR (255) NULL,
+    [FirstName] NVARCHAR (255) NULL,
+    [Address]   NVARCHAR (255) NULL,
+    [City]      NVARCHAR (255) NULL,
+    [CompanyId] INT            NULL
+);
+
